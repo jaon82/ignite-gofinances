3202582e49aec981776daf71a655e15e3cda4d9e
Jean Nunes <jean.nunes@indt.org.br>
2022-01-28 09:26:23 -0400

    Navigation

--- App.tsx
+++ App.tsx
@@ -7,9 +7,10 @@ import {
   Poppins_700Bold as boldFont,
 } from '@expo-google-fonts/poppins';
 import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native';
 
 import theme from './src/global/styles/theme';
-import Register from './src/screens/Register';
+import AppRoutes from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,7 +25,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
